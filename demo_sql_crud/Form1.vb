c459872934c802cb22606e9c0283bd850fc54b4c
jeloing-kod <lkjeloingfd>
2025-11-18 19:38:00 +0800

    failed to connect to my sql

--- demo_sql_crud/Form1.vb
+++ demo_sql_crud/Form1.vb
@@ -1,7 +1,25 @@
-﻿
+﻿Imports MySql.Data.MySqlClient
 
 Public Class Form1
+
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
     Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server = localhost; user id = root; password = root; database = demo_sql_crud"
+
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection Successful")
+            conn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            conn.Close()
+        End Try
+
+
 
     End Sub
 End Class
