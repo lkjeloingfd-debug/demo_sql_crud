7943f87f7ae1552d56f9c8e58b32cb704a6c399b
jeloing-kod <lkjeloingfd>
2025-11-21 19:51:44 +0800

    working on data grid view

--- demo_sql_crud/Form1.Designer.vb
+++ demo_sql_crud/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ButtonConnect = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -30,12 +30,16 @@ Partial Class Form1
         Me.TextBoxEmail = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.ButtonCreate = New System.Windows.Forms.Button()
+        Me.buttonRead = New System.Windows.Forms.Button()
+        Me.DataGridView = New System.Windows.Forms.DataGridView()
+        Me.TextBoxHiddenId = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
         Me.ButtonConnect.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ButtonConnect.Location = New System.Drawing.Point(66, 25)
+        Me.ButtonConnect.Location = New System.Drawing.Point(46, 12)
         Me.ButtonConnect.Name = "ButtonConnect"
         Me.ButtonConnect.Size = New System.Drawing.Size(167, 85)
         Me.ButtonConnect.TabIndex = 0
@@ -103,12 +107,40 @@ Partial Class Form1
         Me.ButtonCreate.Text = "CREATE (Insert Data)"
         Me.ButtonCreate.UseVisualStyleBackColor = True
         '
+        'buttonRead
+        '
+        Me.buttonRead.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonRead.Location = New System.Drawing.Point(257, 266)
+        Me.buttonRead.Name = "buttonRead"
+        Me.buttonRead.Size = New System.Drawing.Size(167, 85)
+        Me.buttonRead.TabIndex = 8
+        Me.buttonRead.Text = "READ(Get Data)"
+        Me.buttonRead.UseVisualStyleBackColor = True
+        '
+        'DataGridView
+        '
+        Me.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView.Location = New System.Drawing.Point(66, 380)
+        Me.DataGridView.Name = "DataGridView"
+        Me.DataGridView.Size = New System.Drawing.Size(836, 195)
+        Me.DataGridView.TabIndex = 9
+        '
+        'TextBoxHiddenId
+        '
+        Me.TextBoxHiddenId.Location = New System.Drawing.Point(136, 103)
+        Me.TextBoxHiddenId.Name = "TextBoxHiddenId"
+        Me.TextBoxHiddenId.Size = New System.Drawing.Size(199, 20)
+        Me.TextBoxHiddenId.TabIndex = 10
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(490, 371)
+        Me.ClientSize = New System.Drawing.Size(914, 608)
+        Me.Controls.Add(Me.TextBoxHiddenId)
+        Me.Controls.Add(Me.DataGridView)
+        Me.Controls.Add(Me.buttonRead)
         Me.Controls.Add(Me.ButtonCreate)
         Me.Controls.Add(Me.TextBoxEmail)
         Me.Controls.Add(Me.Label3)
@@ -119,6 +151,7 @@ Partial Class Form1
         Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -132,4 +165,7 @@ Partial Class Form1
     Friend WithEvents TextBoxEmail As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents ButtonCreate As Button
+    Friend WithEvents buttonRead As Button
+    Friend WithEvents DataGridView As DataGridView
+    Friend WithEvents TextBoxHiddenId As TextBox
 End Class
--- demo_sql_crud/Form1.vb
+++ demo_sql_crud/Form1.vb
@@ -46,4 +46,36 @@ Public Class Form1
 
 
     End Sub
+
+    Private Sub buttonRead_Click(sender As Object, e As EventArgs) Handles buttonRead.Click
+        Dim query As String = "SELECT * FROM crud_demo_db_santillan.students_tbl;"
+        Try
+            Using conn As New MySqlConnection("server = localhost; user id = root; password = root; database = crud_demo_db_santillan")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                DataGridView.DataSource = table
+                DataGridView.Columns("id").Visible = False
+
+            End Using
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DataGridView_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView.CellContentDoubleClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView.Rows(e.RowIndex)
+            TextBoxName.Text = selectedRow.Cells("Name").Value.ToString()
+            TextBoxAge.Text = selectedRow.Cells("age").Value.ToString()
+            TextBoxEmail.Text = selectedRow.Cells("email").Value.ToString()
+            TextBoxHiddenId.Text = selectedRow.Cells("id").Value.ToString()
+
+
+        End If
+    End Sub
+
+
 End Class
