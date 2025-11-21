0aa1faf78095ec6366774f11286a687f8bf3cdc2
jeloing-kod <lkjeloingfd>
2025-11-21 19:11:12 +0800

    created the crud

--- demo_sql_crud/Form1.Designer.vb
+++ demo_sql_crud/Form1.Designer.vb
@@ -23,28 +23,113 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.TextBoxAge = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ButtonCreate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
-        Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
+        Me.ButtonConnect.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonConnect.Location = New System.Drawing.Point(66, 25)
         Me.ButtonConnect.Name = "ButtonConnect"
         Me.ButtonConnect.Size = New System.Drawing.Size(167, 85)
         Me.ButtonConnect.TabIndex = 0
         Me.ButtonConnect.Text = "Connect My SQL"
         Me.ButtonConnect.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(62, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name"
+        '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(136, 135)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(199, 20)
+        Me.TextBoxName.TabIndex = 2
+        '
+        'TextBoxAge
+        '
+        Me.TextBoxAge.Location = New System.Drawing.Point(136, 176)
+        Me.TextBoxAge.Name = "TextBoxAge"
+        Me.TextBoxAge.Size = New System.Drawing.Size(199, 20)
+        Me.TextBoxAge.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(62, 183)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Age"
+        '
+        'TextBoxEmail
+        '
+        Me.TextBoxEmail.Location = New System.Drawing.Point(136, 218)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(199, 20)
+        Me.TextBoxEmail.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(62, 221)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Email"
+        '
+        'ButtonCreate
+        '
+        Me.ButtonCreate.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCreate.Location = New System.Drawing.Point(66, 266)
+        Me.ButtonCreate.Name = "ButtonCreate"
+        Me.ButtonCreate.Size = New System.Drawing.Size(167, 85)
+        Me.ButtonCreate.TabIndex = 7
+        Me.ButtonCreate.Text = "CREATE (Insert Data)"
+        Me.ButtonCreate.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(602, 478)
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(490, 371)
+        Me.Controls.Add(Me.ButtonCreate)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxAge)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxName)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ButtonConnect As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxAge As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxEmail As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonCreate As Button
 End Class
--- demo_sql_crud/Form1.vb
+++ demo_sql_crud/Form1.vb
@@ -7,7 +7,7 @@ Public Class Form1
 
     Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
         conn = New MySqlConnection
-        conn.ConnectionString = "server = localhost; user id = root; password = root; database = demo_sql_crud"
+        conn.ConnectionString = "server = localhost; user id = root; password = root; database = crud_demo_db_santillan"
 
 
         Try
@@ -19,6 +19,30 @@ Public Class Form1
             conn.Close()
         End Try
 
+
+
+    End Sub
+
+    Private Sub ButtonCreate_Click(sender As Object, e As EventArgs) Handles ButtonCreate.Click
+        Dim query As String = "INSERT INTO `crud_demo_db_santillan`.`students_tbl` ( `Name`, `age`, `email`) VALUES ( @Name, @age, @email);"
+        Try
+            Using conn As New MySqlConnection("server = localhost; user id = root; password = root; database = crud_demo_db_santillan")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@age", CInt(TextBoxAge.Text))
+                    cmd.Parameters.AddWithValue("@email", TextBoxEmail.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record Inserted Successfully")
+                    TextBoxName.Clear()
+                    TextBoxAge.Clear()
+                    TextBoxEmail.Clear()
+
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
 
 
     End Sub
